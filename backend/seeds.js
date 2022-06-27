140d4678751b8edd6e212d14a19604c14da07282
Gal Berger <gal.berger@innovid.com>
2022-06-27 13:02:34 +0300

    close connection to db on done

--- backend/seeds.js
+++ backend/seeds.js
@@ -1,4 +1,4 @@
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
 if (!process.env.MONGODB_URI) {
     console.warn("Missing MONGODB_URI in env, please add it to your .env file");
@@ -9,34 +9,37 @@ require("./models/User");
 require("./models/Item");
 require("./models/Comment");
 
-var User = mongoose.model("User");
-var Item = mongoose.model("Item");
-var Comment = mongoose.model("Comment");
+const User = mongoose.model("User");
+const Item = mongoose.model("Item");
+const Comment = mongoose.model("Comment");
 
 console.log("seeding the database with 100 users, items and comments")
-range(0, 100, 1) 
+const promises = range(0, 100, 1) 
     .map(i => {
-        var rand = Math.random().toString(36).slice(2, 7);
-        var user = new User();
+        const rand = Math.random().toString(36).slice(2, 7);
+        const user = new User();
         user.username = "u" + rand;
         user.email = user.username + "@fake.com";
         user.setPassword("hey " + user.username);
 
-        var item = new Item();
+        const item = new Item();
         item.title = "i" + rand;
         item.seller = user;
 
-        var comment = new Comment();
+        const comment = new Comment();
         comment.body = item.title + " is awesome!"
         comment.item = item;
         comment.seller = user;
 
         item.comments.concat([comment]);
 
-        user.save();
-        item.save();
-        comment.save();
+        return user.save()
+            .then(_ => item.save()
+                .then(_ => comment.save())
+            );
     });
+
+Promise.all(promises).then(_ => mongoose.connection.close());
 
 function range (start, stop, step) {
     return Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step));
